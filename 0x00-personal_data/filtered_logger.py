9e218b92528ee87493f98b4eb55ee17192c1b93e
Nick Nyanjui <nicknyanjui@gmail.com>
2023-03-02 01:39:08 +0300

    Add filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Use a regex to replace occurrences of certain field values."""
 
+import logging
 import re
 from typing import List
 
@@ -23,3 +24,23 @@ def filter_datum(fields: List[str], redaction: str,
                              message, re.IGNORECASE)
         message = re.sub(sub.group(0), redaction, message) if sub else None
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(fmt=self.FORMAT)
+        self.fields: list = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        msg: str = filter_datum(self.fields, self.REDACTION,
+                                record.getMessage(), self.SEPARATOR)
+        return (self.FORMAT) % {"name": record.name, "message": msg,
+                                "levelname": record.levelname,
+                                "asctime": self.formatTime(record)}
