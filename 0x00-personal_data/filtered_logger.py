4121042bce1bed6c9fac251de44932703f12a9f9
Nick Nyanjui <nicknyanjui@gmail.com>
2023-03-02 00:52:47 +0300

    Add filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Use a regex to replace occurrences of certain field values."""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Args:
+        fields (list): a list of strings representing all fields to obfuscate
+        redaction (str): a string representing by what the field
+            will be obfuscated
+        message (str): a string representing the log line
+        separator (str): a string representing by which character
+            is separating all fields in the log line (message)
+    Returns:
+        (str): the log message obfuscated
+    """
+    for field in fields:
+        sub: str = re.search(f"(?<={field}=).*?(?=;)", message, re.IGNORECASE)
+        message = re.sub(sub.group(0), redaction, message) if sub else None
+    return message
