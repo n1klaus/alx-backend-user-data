53bec7d30645b636998cf9d6884d781d3332553b
Nick Nyanjui <nicknyanjui@gmail.com>
2023-03-03 00:43:01 +0300

    Add encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,6 +5,12 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
+    """Returns a salted and hashed password byte string"""
     salt: bytes = bcrypt.gensalt()
     hashed: bytes = bcrypt.hashpw(password.encode("utf-8"), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates that the encrypted password matches provided password"""
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
