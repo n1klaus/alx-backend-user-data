ddfeaf43a51d109f8498a7b0f8d690a0ab21f38c
Nick Nyanjui <nicknyanjui@gmail.com>
2023-03-07 01:18:08 +0300

    Add auth class method require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,12 +12,16 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks if authentication is required for path"""
-        return False
-    
+        if path is None or not excluded_paths:
+            return True
+        if (path or f"{path}/") in excluded_paths:
+            return False
+        return True
+
     def authorization_header(self, request=None) -> str:
         """Returns authorization header from a request if available"""
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns authorized user from a request if available"""
         return None
