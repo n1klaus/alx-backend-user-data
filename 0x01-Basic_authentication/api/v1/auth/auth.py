ed6bdcb3017d31a603ba489171a0b7eaa9b967f0
Nick Nyanjui <nicknyanjui@gmail.com>
2023-03-07 00:50:20 +0300

    Add auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module for authentication handling
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Authentication system"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if authentication is required for path"""
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """Returns authorization header from a request if available"""
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns authorized user from a request if available"""
+        return None
