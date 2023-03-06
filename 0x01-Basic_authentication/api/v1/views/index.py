1267e2dcf38f21814c76d4b756c363ce2f2989f0
Nick Nyanjui <nicknyanjui@gmail.com>
2023-03-07 00:22:55 +0300

    Add 401 error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def error_401() -> None:
+    """Raises a 401 error"""
+    abort(401)
