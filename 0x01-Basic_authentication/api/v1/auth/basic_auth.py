ecffd22fcf338e77da127467f88b4a637f96a5fb
Nick Nyanjui <nicknyanjui@gmail.com>
2023-03-07 23:07:04 +0300

    Add BasicAuth method extract_base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,8 +4,18 @@ Module for basic authentication handling
 """
 
 from api.v1.auth.auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """Basic authentication scheme"""
-    pass
+    
+    def extract_base64_authorization_header(self, 
+                                            authorization_header: str) -> str:
+        """Returns base64 part of authorization header"""
+        if authorization_header is None or \
+            not isinstance(authorization_header, str) or \
+                not re.search("Basic\s.*", authorization_header):
+            return None
+        result: str = re.search("(?<=Basic\s).*", authorization_header)
+        return str(result.group(0))
